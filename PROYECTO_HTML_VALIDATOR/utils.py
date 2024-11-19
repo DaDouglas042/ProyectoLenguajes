a6c0d229ee35c038ac3c3390cefbb451a45c3245
lucidotbat-admin <lfernando.rr@outlook.com>
2024-11-18 20:41:24 -0600

    Nuevo bloque para manejo de excepciones cuando no sea posible abrir el archivo html

--- PROYECTO_HTML_VALIDATOR/utils.py
+++ PROYECTO_HTML_VALIDATOR/utils.py
@@ -5,3 +5,6 @@ def read_html_file(file_path):
     except FileNotFoundError:
         print(f"El archivo '{file_path}' no fue encontrado.")
         return ""
+    except Exception as e:
+        print(f"Ocurrió un error al leer el archivo: {e}")
+        return ""
