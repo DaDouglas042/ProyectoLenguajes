9141da8a27232a226b63a14c4055bd33f7d722a9
lucidotbat-admin <lfernando.rr@outlook.com>
2024-11-18 20:35:46 -0600

    Manejo de contenido vacio, reinicio de last_header_level, correccion en importacion de last_header_level

--- PROYECTO_HTML_VALIDATOR/main.py
+++ PROYECTO_HTML_VALIDATOR/main.py
@@ -5,14 +5,21 @@ import utils
 def main():
     file_path = input("Ingresa la ruta del archivo HTML a validar (ej., valid.html): ")
     content = utils.read_html_file(file_path)
+    
+    if not content:
+        return
+
     print("\n--- Análisis Léxico ---\n")
     
     # Análisis léxico
     lexer.input(content)
     for token in lexer:
         print(token)
-
+    
     print("\n--- Análisis Sintáctico ---\n")
+    # Reiniciar el nivel del último encabezado antes del análisis sintáctico
+    from parser import last_header_level
+    last_header_level = 0
     # Análisis sintáctico
     parser.parse(content)
 
