c9be2781089ca9a2b53badfdd1514cfefc43e0b8
lucidotbat-admin <lfernando.rr@outlook.com>
2024-11-18 20:46:37 -0600

    Validacion de secuencia de encabezados, condicion para asegurar que encabezados tienen orden correcto en p_header y simplificacion de reglas gramaticales

--- PROYECTO_HTML_VALIDATOR/parser.py
+++ PROYECTO_HTML_VALIDATOR/parser.py
@@ -1,23 +1,26 @@
 import ply.yacc as yacc
 from lex import tokens
 
+# Variable global para rastrear el nivel del último encabezado
+last_header_level = 0
+
 # Gramática
 def p_structure(p):
     '''structure : header paragraphs structure
-                 | header paragraphs
                  | header structure
+                 | header paragraphs
                  | header
     '''
 
 def p_header(p):
-    '''header : H1
-              | H2
-              | H3
-              | H4
-              | H5
-              | H6
-    '''
-    print(f"Encabezado encontrado: {p[1]}")
+    'header : HEADER'
+    global last_header_level
+    current_level = int(p[1][2:-1])  # Extrae el número del encabezado
+    if current_level != last_header_level + 1:
+        print(f"Error: Encabezado fuera de secuencia en {p[1]}")
+    else:
+        print(f"Encabezado correcto: {p[1]}")
+    last_header_level = current_level
 
 def p_paragraphs(p):
     '''paragraphs : P paragraphs
